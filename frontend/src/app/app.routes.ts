03574480af7b69de046dfaffeb05c98ea5f88bd9
Manasa Kallam <manasauni02@gmail.com>
2026-02-10 11:08:14 -0500

    NEW: Added routing for login, dashboard, units, tenants

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Units } from './pages/units/units';
+import { Tenants } from './pages/tenants/tenants';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+    { path: 'login', component: Login },
+    { path: 'dashboard', component: Dashboard },
+    { path: 'units', component: Units },
+    { path: 'tenants', component: Tenants },
+
+    { path: '**', redirectTo: 'login' }
+];
---
+++ frontend/src/app/pages/units/units.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-units',
+  imports: [RouterLink],
+  templateUrl: './units.html',
+  styleUrl: './units.scss',
+})
+export class Units {
+
+}
