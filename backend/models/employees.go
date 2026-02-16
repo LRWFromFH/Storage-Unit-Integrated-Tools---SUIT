c19d11e845a36163b0a585b16e95593360b5278b
whysuman <sumanyrocket@gmail.com>
2026-02-15 19:46:32 -0500

    feat(auth): add password hashing, JWT tokens and input validation
    1. Added password hashing and matching during the registration and login flows
    2. Added input models for regsitration and login inputs.

--- backend/models/employees.go
+++ backend/models/employees.go
@@ -6,6 +6,17 @@ type Employee struct {
 	gorm.Model
 	SMID     string `json:"username" gorm:"unique"` //System Member ID
 	Email    string `json:"email" gorm:"unique"`    //Company Email
-	Password string `json:"password"`
+	Password string `json:"-"`
 	Role     string `json:"role"`
+}
+
+type RegisterRequest struct {
+	SMID     string `json:"username" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=8"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
 }
