d458ce1875ca31dfaa8ddd640d428853cf4e07a3
Alexander Martin <LordRichardWellington@live.com>
2026-02-16 13:09:19 -0500

    Middleware: cors - Config needs to be adjusted.

--- backend/main.go
+++ backend/main.go
@@ -4,11 +4,18 @@ import (
 	"backend/database"
 	"backend/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:4200"}
+	config.AllowCredentials = true
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Accept", "Authorization"}
+	r.Use(cors.New(config))
 
 	database.Connect()
 	routes.SetupRoutes(r)
